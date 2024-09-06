f65a21819a491b4b90f96442ff390557ef714c39
polathip-sint <polathip.sint@outlook.com>
2024-09-06 09:26:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelection('.navlist');
 
+const sr = ScrollReveal ({
+    distance: '65px',
+    duration: 2600,
+    delay: 450,
+    reset: true
+});
+
+sr.reveal('.description',{delay:200, origin:'top'});
+sr.reveal('FLOWBOTs-img',{delay:450, origin:'top'});
